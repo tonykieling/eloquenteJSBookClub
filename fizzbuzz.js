ae5ac2fba19e875c2865ebffc16666b06bc0dc40
Tony Kieling <tony.kieling@gmail.com>
2020-07-27 20:39:28 -0700

    first and second meeting

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+const 
+  intial  = 1,
+  final   = 100;
+
+for(count = intial; count <= final; count++){
+  if ((count % 3 === 0) && (count % 5 === 0))
+    console.log(`FizzBuzz (${count})`);
+  else if (count % 3 === 0)
+    console.log(`Fizz (${count})`);
+  else if (count % 5 === 0)
+    console.log(`Buzz (${count})`);
+  else
+    console.log(`- ${count}`);
+}

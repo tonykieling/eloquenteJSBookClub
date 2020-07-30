d18979196ce7e72aa3eb7c14146b408677fdf315
Tony Kieling <tony.kieling@gmail.com>
2020-07-29 20:21:53 -0700

    small changes - meeting 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,13 +2,15 @@ const
   intial  = 1,
   final   = 100;
 
-for(count = intial; count <= final; count++){
-  if ((count % 3 === 0) && (count % 5 === 0))
+for(let count = intial; count <= final; count++){
+
+  if ((count % 3 === 0) && (count % 5 === 0)) {
     console.log(`FizzBuzz (${count})`);
-  else if (count % 3 === 0)
+  } else if (count % 3 === 0)
     console.log(`Fizz (${count})`);
   else if (count % 5 === 0)
     console.log(`Buzz (${count})`);
   else
     console.log(`- ${count}`);
+
 }

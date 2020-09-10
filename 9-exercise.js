167d9c305a2b172819cf96f4d876abe2b815394d
Tony Kieling <tony.kieling@gmail.com>
2020-09-09 19:46:18 -0700

    chapter 9 is done

--- 9-exercise.js
+++ 9-exercise.js
@@ -1,23 +1,11 @@
-// I'm = I am
-// You're = You are
-// He's = he is
-// She's = she is
-
-// I'm   => I am
-// It's  => It is
-
 // Text: "'I'm the cook,' he said, 'it's my job.'"
 // Output: "I'm the cook," he said, "it's my job."
 
 
-const text = `"'I'm the cook,' he said, 'it's my job.'"`;
-// const re = /'([^']*|'([s]*))'/g;
-// const re = /(["']) | ([.'])/g;
-// const re = /(["'])/g;
-const re = /(\"\')/g;
-console.log(re.exec(text));
-// const output = text.replace(/(\"'), (\")/g, "$2 $1");
-const output = text.replace(/(["'])/, "\"");
-console.log(text);
-console.log(output);
-// console.log(`${text}\n${output}`);
+const text = `"'I'm the cook,' he said, 'it's my job.'"
+"'He's the janitor,' she said, 'it's his job.'"
+"'They're the doctor,' he said, 'it's their job.'"`;
+
+const output = text.replace(/"'/g, "\"").replace(/.'"/g, "\.\"").replace(/\,\'/g, "\,\"").replace(/ \'/g, " \"");
+
+console.log(`--- ORIGINAL TEXT:\n${text}\n\n--- CHANGED:\n${output}`);

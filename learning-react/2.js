459939c7023becf2280fb00f38d4b3168f54b72b
Tony Kieling <tony.kieling@gmail.com>
2020-11-11 18:49:09 -0800

    starting book, chapter 2nd

---
+++ learning-react/2.js
@@ -0,0 +1,56 @@
+// /**
+//  * Function Declaration
+//  * It's hosted and it's possible to call it from anywhere in the program (before or after its declaration)
+//  */
+// console.log(sayHi("Bob"));
+// function sayHi(name){
+//   console.log(this);
+//   return(`Hi ${name}`);
+// }
+// console.log(sayHi("Bob"));
+
+// /**
+//  * Function Expression
+//  * It's NOT hosted - only possible to call it after its declaration
+//  */
+// // console.log(sayHi("Bob")); // ###ERROR
+// const sayHi = function(name, message = "Hello"){
+//   console.log(this);
+//   return(`${message} ${name}`);
+// }
+// console.log(sayHi("Bob"));
+
+/**
+ * Arrow Function
+ * It's NOT hosted - only possible to call it after its declaration + THIS has a diff value
+ */
+// console.log(sayHi("Bob")); // ###ERROR
+// const sayHi = (name, message = "Hello") => {
+//   console.log(this);
+//   return(`${message} ${name}`);
+// }
+// OR
+// const sayHi = (name, message = "Hello") => `${message} ${name}`;
+// console.log(sayHi("Bob", "Hey my friend"));
+// console.log(sayHi());
+
+// const person = (firstName, lastName) => 
+// // {"message": "asd"} // it gives an ERROR // the solution is warpping the object with parentheses
+//     ({
+//         first: firstName,
+//         last: lastName
+//     })
+
+// console.log(person("Brad", "Janson"));
+
+
+// const tahoe = {
+//   mountains: ["Freel", "Rose", "Tallac", "Rubicon", "Silver"],
+//   print: function(delay = 500) {
+//     setTimeout(() => { // this function will access 'this' from the upper print function, which is mountains
+//       console.log(this.mountains.join(", "));
+//     }, delay);
+//   }
+// };
+
+// tahoe.print(); // Uncaught TypeError: Cannot

31fceb7f8d1a89c8a63d05e0fb14017ac249dedd
Tony Kieling <tony.kieling@gmail.com>
2021-02-04 11:49:30 -0800

    practicing more AJAX

--- learning-react/3/basic-react/doing-ajax.js
+++ learning-react/3/basic-react/doing-ajax.js
@@ -1,33 +1,29 @@
-// it needs to be running on a server (live)
+// it needs to be running on a server (VSCode live)
 
-// const API1 = "local-file.txt";
-const API2 = "https://jsonplaceholder.typicode.com/todos/1";
-// const API3 = "https://loripsum.net/api/5/short/headers/prude/decorate";
+// const API = "local-file.txt";
+const API = "https://jsonplaceholder.typicode.com/todos/1";
+// const API = "https://loripsum.net/api/5/short/headers/prude/decorate";
 
-// const listOfLibAndFrameworks = ['React.js', 'Angular', 'Vue.js', 'Node.js', 'underscore.js'];
 
 function ajaxQuery(){
   const ajaxConnection = new XMLHttpRequest();
-  ajaxConnection.open("GET", API2, true);
-
-  // ajaxConnection.mode = "no-cors";
-  // ajaxConnection.withCredentials = true
-  // ajaxConnection.setRequestHeader('Content-Type', 'application/json')
+  ajaxConnection.open("GET", API, true);
   
   ajaxConnection.onload = function() {
     // onload function will run when .status property is equal to 4 - ready status.
-    console.log("ajaxConnection.readyState:", ajaxConnection.readyState, ajaxConnection.status);
-    console.log("ajaxConnection.responseText", this.responseText);
+
     if (ajaxConnection.status == 200) {
-      console.log("this.responseText.title", JSON.parse(this.response).title);
-      const result = JSON.parse(this.response);
+      // console.log("this.responseText.title", JSON.parse(this.response));
+      console.log("this.responseText.title", this.response);
+      // const result = JSON.parse(this.response);
+      const result = this.response;
 
       const destiny = document.querySelector("#main-div");
+
 
       // using React
       const newElement = React.createElement("p", {}, result.title);
       ReactDOM.render(newElement, destiny);
-
 
       // NO react
       // const newElement = document.createElement("p");
@@ -46,7 +42,45 @@ const mainAJAXReactElement = React.createElement(
     // React.createElement( "div", {id: "main-div", style: { border: "solid 1px red"}}, [])
     React.createElement( "div", {id: "main-div"}, [])
 );
+
+
+
+const test = () => {
   
-console.log( "what is the react element 'mainReactElement':", mainAJAXReactElement );
+  // const myRequest = new XMLHttpRequest()
+  // myRequest.open("GET", "https://loripsum.net/api/5/short/headers/prude/decorate");
+  
+  // myRequest.setRequestHeader("Access-Control-Allow-Origin", "*");
+  
+  // myRequest.onload = () => console.log("myRequest.response ===>", myRequest.response);
+  
+  // myRequest.send();
+  
+
+  // const myHeaders = new Headers();
+  // myHeaders.append("Access-Control-Allow-Origin", "*");
+  // myHeaders.append("Access-Control-Request-Headers", "Content-Type, Accept");
+  // myHeaders.append("Content-Type", "application/json");
+  
+  const myRequest = new Request(API, {
+    method: 'GET',
+    // headers: myHeaders,
+    mode: 'no-cors'
+  });
+  
+  fetch(myRequest)
+    // .then(r => r.json())
+    .then(response => console.log("response::", response))
+
+
+
+  // fetch(myRequest)
+  //   .then(response => response.json())
+  //   // .then(data => console.log(data));
+
+}
+
+test();
+
   
 ReactDOM.render(mainAJAXReactElement, document.querySelector( '#root' ));

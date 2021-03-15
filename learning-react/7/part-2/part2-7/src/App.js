a05d1dab61d84f4d451a2a4526c291dd37fd51fb
Tony Kieling <tony.kieling@gmail.com>
2021-03-15 11:10:07 -0700

    doing 7th part-2 - answering the questionnaire and practicing code

---
+++ learning-react/7/part-2/part2-7/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Comp1 from "./components/comp1.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Comp1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ learning-react/7/part-2/part2-7/src/components/comp1.js
@@ -0,0 +1,90 @@
+// first way
+// import { useState } from "react";
+// export default function Comp1() {
+//   const [checked, setChecked] = useState(false);
+
+//   return (
+//     <>
+//       <span>Is it checked? </span>
+//       <input
+//         type = "checkbox"
+//         value = { checked }
+//         onChange = { () => setChecked(checked => !checked) }
+//       />
+//       { checked ? "YES" : "NO"}
+//     </>
+//   );
+// }
+
+
+// second way
+// import { useState } from "react";
+
+// export default function Comp1() {
+//   const [checked, setChecked] = useState(false);
+
+//   const toggle = () => {
+//     setChecked(checked => !checked)
+//   };
+
+//   return (
+//     <>
+//       <span>Is it checked? </span>
+//       <input
+//         type = "checkbox"
+//         value = { checked }
+//         onChange = { toggle }
+//       />
+//       { checked ? "YES" : "NO"}
+//     </>
+//   );
+// }
+
+
+// // third way
+// // setChecked is defined in the useReducer, while checked is the second parameter of useReducer
+// import { useReducer } from "react";
+
+// export default function Comp1() {
+//   const [checked, setChecked] = useReducer(checked => !checked, false);
+
+//   return (
+//     <>
+//       <span>Is it checked? </span>
+//       <input
+//         type = "checkbox"
+//         value = { checked }
+//         onChange = { setChecked }
+//       />
+//       { checked ? "YES" : "NO"}
+//     </>
+//   );
+// }
+
+
+// fourth way
+// another usage of useReducer
+import { useReducer, useState } from "react";
+
+export default function Comp1() {
+  const [number, setNumber] = useReducer((newNumber, currentNumber) => Number(newNumber) + Number(currentNumber), 0);
+
+  const [toAdd, setToAdd] = useState(0);
+
+  return (
+    <>
+      <span>How much to add? </span>
+      <input
+        type = "number"
+        value = { toAdd }
+        onChange = { e => setToAdd(e.target.value) }
+      />
+      <button
+        onClick = { () => setNumber(toAdd, number) }
+      >
+        Add
+      </button>
+        { ` ${number}` }
+    </>
+  );
+}
